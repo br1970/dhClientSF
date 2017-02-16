2dc81404734ead96b0d33fdbd565cd7488b9c0c6
Ricardo Balduino <balduino@us.ibm.com>
2017-02-16 10:45:50 -0800

    changes to fit my repositories

--- deploy/sf/upgrade.sh
+++ deploy/sf/upgrade.sh
@@ -21,7 +21,7 @@ sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/"
 [ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
 echo "---Updating CodePackage Completed"
 echo "---Updating ImageName.."
-sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>knizami\/dhclient:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhClient/ServiceManifest.xml
+sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>balduino\/dhclientsf:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
 echo "---Updating ImageName Completed"
 echo "================================================="
@@ -44,5 +44,5 @@ azure servicefabric application type register dhclient
 echo "================================================="
 echo "Step 5:  Create Application"
 echo "================================================="
-azure servicefabric application upgrade start --application-name fabric:/dh --target-application-type-version $BUILD_BUILDNUMBER --rolling-upgrade-mode Monitored 
+azure servicefabric application upgrade start --application-name fabric:/dh --target-application-type-version $BUILD_BUILDNUMBER --rolling-upgrade-mode Monitored
 [ $? -eq 0 ] || { echo "Failed to create application, Exiting..."; exit 1; }
