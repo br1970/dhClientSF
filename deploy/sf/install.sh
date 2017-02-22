e5778ce188746ff775a1bdd2cf6a4bef8b27a0c4
Ricardo Balduino <balduino@us.ibm.com>
2017-02-22 15:48:37 -0800

    test

--- deploy/sf/install.sh
+++ deploy/sf/install.sh
@@ -5,23 +5,23 @@ echo "================================================="
 echo "Step 1:  Update the manifest versions to: $BUILD_BUILDNUMBER"
 echo "================================================="
 echo "---Updating ApplicationManifest.."
-sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILD_BUILDNUMBER\"/" dhClientPkg/ApplicationManifest.xml
+sed -i "/<ApplicationManifest.*ApplicationTypeVersion/s/ApplicationTypeVersion=\".*\"/ApplicationTypeVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update Application Manifest, Exiting..."; exit 1; }
 echo "---Updating ApplicationManifest Completed"
 echo "---Updating ServiceManifestRef.."
-sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILD_BUILDNUMBER\"/" dhClientPkg/ApplicationManifest.xml
+sed -i "/<ServiceManifestRef.*ServiceManifestVersion/s/ServiceManifestVersion=\".*\"/ServiceManifestVersion=\"$BUILD_BUILDNUMBER\"/" dhclient/ApplicationManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ServiceManifestRef, Exiting..."; exit 1; }
 echo "Updating ServiceManifestRef Completed"
 echo "---Updating ServiceManifest.."
-sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhClientPkg/dhClientPkg/ServiceManifest.xml
+sed -i "/<ServiceManifest.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
 echo "---Updating ServiceManifest Completed"
 echo "---Updating CodePackage.."
-sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhClientPkg/dhClientPkg/ServiceManifest.xml
+sed -i "/<CodePackage.*Version/s/Version=\".*\"/Version=\"$BUILD_BUILDNUMBER\"/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update CodePackage, Exiting..."; exit 1; }
 echo "---Updating CodePackage Completed"
 echo "---Updating ImageName.."
-sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>balduino\/dhclientsf:$BUILD_BUILDNUMBER<\/ImageName>/" dhClientPkg/dhClientPkg/ServiceManifest.xml
+sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>knizami\/dhclient:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
 echo "---Updating ImageName Completed"
 echo "================================================="
@@ -34,15 +34,15 @@ azure servicefabric cluster connect $SERVICEFABRICCLUSTER
 echo "================================================="
 echo "Step 3:  Copy to ImageStore"
 echo "================================================="
-azure servicefabric application package copy dhClientPkg fabric:ImageStore
+azure servicefabric application package copy dhclient fabric:ImageStore
 [ $? -eq 0 ] || { echo "Failed to copy to ImageStore, Exiting..."; exit 1; }
 echo "================================================="
 echo "Step 4:  Register Application"
 echo "================================================="
-azure servicefabric application type register dhClientPkg
+azure servicefabric application type register dhclient
 [ $? -eq 0 ] || { echo "Failed to register application, Exiting..."; exit 1; }
 echo "================================================="
 echo "Step 5:  Create Application"
 echo "================================================="
-azure servicefabric application create fabric:/dhMobile dhMobileType $BUILD_BUILDNUMBER
+azure servicefabric application create fabric:/dh dreamhomesf $BUILD_BUILDNUMBER
 [ $? -eq 0 ] || { echo "Failed to create application, Exiting..."; exit 1; }
