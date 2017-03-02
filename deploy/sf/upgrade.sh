accf78d7eda6a01f27c03f53f963df006e448dde
Ricardo Balduino <balduino@us.ibm.com>
2017-03-02 08:42:34 -0800

    test

--- deploy/sf/upgrade.sh
+++ deploy/sf/upgrade.sh
@@ -28,7 +28,7 @@ echo "---Updating CodePackage Completed"
 echo "---Updating ImageName.."
 sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>balduino\/dhclientsf:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhClient/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
-sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>balduino\/dhclientsf:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhNotification/ServiceManifest.xml
+sed -i "s/<ImageName>.*<\/ImageName>/<ImageName>balduino\/dhnotificationsf:$BUILD_BUILDNUMBER<\/ImageName>/" dhclient/dhNotification/ServiceManifest.xml
 [ $? -eq 0 ] || { echo "Failed to update ImageName, Exiting..."; exit 1; }
 echo "---Updating ImageName Completed"
 echo "================================================="
